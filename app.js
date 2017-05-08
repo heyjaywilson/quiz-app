a861d0e17d0a9e8e462270f99d03dfe4ddcafd6b
maeganjwilson <maegan@maeganwilson.com>
2017-05-08 16:57:20 -0400

    Working on outline of everything still

---
+++ app.js
@@ -0,0 +1,27 @@
+var qAnda = [
+  {
+    question: 'What super hero team is Captain America apart of?',
+    answers: ['The Avengers', 'The Justice League', 'The Power Puff Girls']
+    correctanswer: 0
+  },
+  {
+    question: 'Who is Batman\'s main enemy?',
+    answers: ['The Joker', 'The Penguin', 'Dr. Freeze'],
+    correctanswer: 0
+  },
+  {
+    question: 'Bruce Banner is the human identity for which super hero?',
+    answers: ['Spiderman', 'Ironman', 'The Hulk'],
+    correctanswer: 2
+  },
+  {
+    question: 'Who powers Iron Man\'s suit?',
+    answers: ['Jarvis', 'Skynet', 'Siri'],
+    correctanswer: 0
+  },
+  {
+    question: 'What is Super Man\'s greatest weakness?',
+    answers: ['Love','Kryptonite','Louis Lane']
+    correctanswer: 1
+  }
+];
