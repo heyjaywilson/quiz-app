edc4368b436e197c9c06854d956475084bbc75df
maeganjwilson <maegan@maeganwilson.com>
2017-05-15 10:44:09 -0500

    [ ] Add next question [ ] output scores and progress

--- js/app.js
+++ js/app.js
@@ -1,82 +1,76 @@
-/*=============================================>>>>>
-= GLOBAL VARIABLES =
-===============================================>>>>>*/
-
-var qAndA = [
-  {
-    question: 'What super hero team is Captain America apart of?',
-    answers: ['The Avengers', 'The Justice League', 'The Power Puff Girls'],
-    correctanswer: 0
-  },
-  {
-    question: 'Who is Batman\'s main enemy?',
-    answers: ['The Joker', 'The Penguin', 'Dr. Freeze'],
-    correctanswer: 0
-  },
-  {
-    question: 'Bruce Banner is the human identity for which super hero?',
-    answers: ['Spiderman', 'Ironman', 'The Hulk'],
-    correctanswer: 2
-  },
-  {
-    question: 'Who powers Ironman\'s suit?',
-    answers: ['Jarvis', 'Skynet', 'Siri'],
-    correctanswer: 0
-  },
-  {
-    question: 'What is Superman\'s greatest weakness?',
-    answers: ['Love','Kryptonite','Louis Lane'],
-    correctanswer: 1
-  }
+const QANDA = [
+	{QUEST: 'What super hero team is Captain America apart of?',
+	 ANS: ['The Avengers', 'The Justice League', 'The Power Puff Girls'],
+	 CORRECTANS: 0},
+	{QUEST: 'Who is Batman\'s main enemy?',
+	 ANS: ['The Joker', 'The Penguin', 'Dr. Freeze'],
+	 CORRECTANS: 0},
+	{QUEST: 'Bruce Banner is the human identity for which super hero?',
+	 ANS: ['Spiderman', 'Ironman', 'The Hulk'],
+	 CORRECTANS: 2},
+	{QUEST: 'Who powers Ironman\'s suit?',
+	 ANS: ['Jarvis', 'Skynet', 'Siri'],
+	 CORRECTANS: 0},
+	{QUEST: 'What is Superman\'s greatest weakness?',
+	 ANS: ['Love', 'Kryptonite', 'Louis Lane'],
+	 CORRECTANS: 1}
 ];
-var score = 0;
-var scoreKeep = 'Score '+score+' out of 10 possible';
-var questKeep = 'Question '+questNum+' out of 5';
-var restart = false;
-
-/*= End of GLOBAL VARIABLES =*/
-/*=============================================<<<<<*/
-
-/*=============================================>>>>>
-= Handling scores =
-===============================================>>>>>*/
-
-function isAnswerRight(questNum, ans) {
-  if(ans === qAndA[questNum].correctanswer){
-    score++;
-  }
+var currentScore = 0;
+var userAnswer = 0;
+function show(elm){
+  elm.removeClass('hidden');
+}
+function hide(elm){
+  elm.addClass('hidden');
+}
+function giveQuestion(qIndex){
+	return QANDA[qIndex].QUEST;
+}
+function giveAnsChoice(qIndex, aIndex){
+	return QANDA[qIndex].ANS[aIndex];
+}
+function getCAns(qIndex){
+	return QANDA[qIndex].CORRECTANS;
+}
+function scoreIncrease(cScore){
+	return cScore + 1;
+}
+function checkAns(input, cAns, elm){
+	if(input == cAns){
+		currentScore = scoreIncrease(currentScore);
+		elm.html('You got the question correct and increased your score by 1!');
+	}
+	else{
+		elm.html('You got the question wrong. I\'m sorry.');
+	}
+}
+function renderQuestion(index, elm){
+	elm.html(giveQuestion(index));
+}
+function renderAnswers(index, elm){
+	var answer = '<form>';
+	for (var i = 0; i<3; i++){
+		answer += '<input type ="radio" name="answer" value= "'+i+'">'+giveAnsChoice(index, i)+'</input><br>';
+	}
+	answer += '<button type="submit">Did I get the answer right?</button>';
+	answer += '</form>';
+	elm.append(answer);
+}
+function renderAll(index, elm){
+  renderQuestion(index, elm);
+  renderAnswers(index, elm);
+  $('button').on('click', function(){
+    event.preventDefault();
+    userAnswer = $('input[name="answer"]:checked').val();
+    checkAns(userAnswer, getCAns(index), elm);
+    return index+1;
+  });
 }
 
-function resetScore(){
-  score = 0;
-}
-
-/*= End of Handling scores =*/
-/*=============================================<<<<<*/
-/*=============================================>>>>>
-= CHANGING DISPLAY =
-===============================================>>>>>*/
-
-function hideStart(){
-  $('.start').addClass('hidden');
-}
-
-function showStart(){
-  $('.start').removeClass('hidden');
-}
-
-function showQuestions(){
-  $('ul').removeClass('hidden');
-}
-
-/*=============================================>>>>>
-= Rendering Output =
-===============================================>>>>>*/
-/*=============================================>>>>>
-= EVENT LISTENERS =
-===============================================>>>>>*/
-
-$('.start').on('click', function(event){
-  hideStart();
-  showQuestions();
+$('.start').on('click', function(event) {
+  event.preventDefault();
+  hide($('.start'));
+  show($('.js-box'));
+	var questionNum = 0;
+  questionNum = renderAll(questionNum, $('.questions'));
 });
